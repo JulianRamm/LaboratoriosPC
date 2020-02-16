d098eceb07f7426aad4a109ec8e5e5deb187d261
Julián <juliyanes530@gmail.com>
2020-02-15 22:29:04 -0500

    lab

--- Laboratorio2.py
+++ Laboratorio2.py
@@ -34,11 +34,62 @@ res = ""
 if n / 1 > n // 1 or not(-2**(n-1) <= N <= (2**(n-1))-1):
     print("Error garrafal: el número es racional o el número de bits no es suficiente")
 else:
-    acal = N + 2**n -1
+    acal = int(N) + 2**(n-1)
     while acal >= 1:
         res = str(acal % 2) + res
-        acal = compl // 2
+        acal = acal // 2
     res = "0"*(n-len(res)) + res
-    print("El valor de {} en complemento a 2 es: {}".format(N, res))
+    print("El valor de {} en complemento a 2 es: {}".format(int(N), res))
+## Punto 4
+N = float(input("ingrese el número  a convertir"))
+res = ""
+ent = int(N)
+rac = N - int(N)
+entm = ""
+potencia = ""
+mantissa = ""
+if N > 0:
+    res += "0"
+else:
+    res += "1"
+while ent >= 1:
+    entm = str(ent % 2) + entm
+    ent = ent // 2
+for i in range(24 - len(entm)):
+    rac = rac*2
+    mantissa = mantissa + str(int((rac//1) % 2))
+    if rac > 1:
+        rac = rac - 1
+potencian = len(entm) + 126
+while potencian >= 1:
+    potencia = str(potencian % 2) + potencia
+    potencian = potencian // 2
+print(res, potencia, entm[1:] + mantissa)
+print("El número {} en formato IEE 754 es: {}".format(N, res + potencia + entm[1:] + mantissa))
+## Punto 5
+N = float(input("ingrese el número  a convertir"))
+res = ""
+ent = int(N)
+rac = N - int(N)
+entm = ""
+potencia = ""
+mantissa = ""
+if N > 0:
+    res += "0"
+else:
+    res += "1"
+while ent >= 1:
+    entm = str(ent % 2) + entm
+    ent = ent // 2
+for i in range(53 - len(entm)):
+    rac = rac*2
+    mantissa = mantissa + str(int((rac//1) % 2))
+    if rac > 1:
+        rac = rac - 1
+potencian = len(entm) + 1022
+while potencian >= 1:
+    potencia = str(potencian % 2) + potencia
+    potencian = potencian // 2
+print("El número {} en formato IEE 754 es: {}".format(N, res + potencia + entm[1:] + mantissa))
 ##
 
