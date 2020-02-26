c791094f43d4aa50a236973cf7a7801da8554a12
Julián <juliyanes530@gmail.com>
2020-02-25 22:15:04 -0500

    hasta el punto 9

--- Laboratorio3.py
+++ Laboratorio3.py
@@ -20,7 +20,7 @@ def esPrimo(n):
         if n % j == 0:  # En esta línea se calcula el residuo de la división de i con j. Donde i es el i-esimo número de la lista y j es un número menor a la raíz de i
             return False
     return True
-##
+## Punto 4
 def esPalindrome(cad):
         reemplazos = (
             ("á", "a"),
@@ -28,10 +28,64 @@ def esPalindrome(cad):
             ("í", "i"),
             ("ó", "o"),
             ("ú", "u"),
+            (" ", ""),
         )
         for a, b in reemplazos:
             cad = cad.replace(a, b).replace(a.upper(), b.upper())
-        return cad.upper() ==
-print(esPalindrome("Halí"))
-##
+        cad = cad.upper()
+        return cad == cad[::-1]
+## Punto 5
+import math as m
+def pertFib(n):
+    pro1 = 5*n*n+4
+    par1 = int(m.sqrt(pro1))
+    pro2 = 5*n*n-4
+    par2 = int(m.sqrt(pro2))
+    return par1*par1==pro1 or par2*par2 == pro2
+## Punto 6
+def calcMedia(arr):
+    suma = 0
+    n = len(arr)
+    for i in range(n):
+        suma += arr[i]
+    return suma / n
+def mediasFilas(arr):
+    resp = []
+    for i in range(len(arr)):
+        resp[i] = calcMedia(arr[i])
+    return resp
+## Punto 7
+def maxArr(arr):
+    max = 0
+    for i in arr:
+        if i>max:
+            max = i
+    return max
+def maxsFilas(arr):
+    resp = []
+    for i in range(len(arr)):
+        resp[i] = maxArr(arr[i])
+    return resp
+## Punto 8
+import math as m
+def minArr(arr):
+    min = m.inf
+    for i in arr:
+        if i<min:
+            min = i
+    return min
+def minsFilas(arr):
+    resp = []
+    for i in range(len(arr)):
+        resp[i] = minArr(arr[i])
+    return resp
+## Punto 9
+def sumaMatrices(mat1, mat2):
+    resp = [[]]
+    for i in len(mat1):
+        for j in len(mat1[0]):
+            resp[i][j] = mat1[i][j] + mat2[i][j]
+    return resp;
+## Punto 10
+
 
